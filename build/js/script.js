aedb8fd09bc38e786708183e56fb5be076780ab8
Dmitriy <demianko@mail.ru>
2019-05-25 08:41:21 +0300

    refactoring in script.js was rolled back because toggle button doesn't work properly.

--- build/js/script.js
+++ build/js/script.js
@@ -5,15 +5,15 @@ const map = document.querySelector(".hotel-search__map-wraper");
 
 toggle.addEventListener("click", function (evt) {
     evt.preventDefault();
-    // if (navig.classList.contains("navig--closed")) {
-    //     navig.classList.remove("navig--closed");
-    //     navig.classList.add("navig--opened");
-    // } else {
-    //     navig.classList.remove("navig--opened");
-    //     navig.classList.add("navig--closed");
-    // }
-    navig.classList.toggle("navig--opened");
-    navig.classList.toggle("navig--closed");
+    if (navig.classList.contains("navig--closed")) {
+        navig.classList.remove("navig--closed");
+        navig.classList.add("navig--opened");
+    } else {
+        navig.classList.remove("navig--opened");
+        navig.classList.add("navig--closed");
+    }
+    // navig.classList.toggle("navig--opened");
+    // navig.classList.toggle("navig--closed");
 });
 map.addEventListener("click",function (evt) {
     evt.preventDefault();
